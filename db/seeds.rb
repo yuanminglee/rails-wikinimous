ac617ef330c32bf84d97853d9c0cae2a4453f84a
YuanMing Lee <yuanming.lee91@gmail.com>
2021-02-10 21:33:23 +0800

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Movies::BackToTheFuture.character,
+    content: Faker::Movies::BackToTheFuture.quote
+  )
+end
